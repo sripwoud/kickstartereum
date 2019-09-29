228a0ff71b5a66168cbff92eafa043609ded440d
sripwoud <me@sripwoud.xyz>
2019-09-29 23:28:13 +0200

    refactor: Use dotenv for environment variables

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,16 +1,14 @@
+require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 const Web3 = require('web3')
 const fs = require('fs-extra')
-const path = require('path')
 
+const path = require('path')
 const { abi, evm: { bytecode: { object: bytecode } } } = require('./build/CampaignFactory.json')
 
-const mnemonic = fs.readFileSync('./.mnemonic').toString().trim()
-const infuraKey = fs.readFileSync('./.infuraKey').toString().trim()
-
 const provider = new HDWalletProvider(
-  mnemonic,
-  `https://rinkeby.infura.io/${infuraKey}`
+  process.env.MNEMONIC,
+  `https://rinkeby.infura.io/${process.env.INFURA_KEY}`
 )
 const web3 = new Web3(provider)
 // define function so that we can use await/async syntax (optional)
@@ -24,7 +22,22 @@ const deploy = async () => {
       .send({ from: accounts[0], gas: '1800000' })
 
     console.log('Contract deployed to', factory.options.address)
-    fs.outputFile(path.resolve(__dirname, 'ADDRESS'), factory.options.address)
+    fs.readFile(
+      path.resolve(__dirname, '.env'),
+      'utf8',
+      (err, data) => {
+        if (!err) {
+          const splitArray = data.split('\n')
+          if (splitArray.length === 4) {
+            splitArray.splice(2, 2)
+          }
+          splitArray.push(`ADDRESS=${factory.options.address}`)
+          const result = splitArray.join('\n')
+          fs.writeFile(path.resolve(__dirname, '.env'), result)
+        } else {
+          console.log(err)
+        }
+      })
   } catch (e) {
     console.log('Failed to deploy:', e.message)
   }
