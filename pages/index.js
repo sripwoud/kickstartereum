86dd095a50e4586b900b0edafaf780bf400356af
Gauthier Riou <gauthier.riou@airbus.com>
2019-09-30 15:48:20 +0200

    Add Layout component to all pages

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import instance from '../ethereum/factory'
 import { Card, Button } from 'semantic-ui-react'
 
+import Layout from '../components/Layout'
+
 class CampaignIndex extends Component {
   // static async getInitialProps () {
   //   const campaigns = await instance.methods.getDeployedCampaigns().call()
@@ -21,15 +23,18 @@ class CampaignIndex extends Component {
 
   render () {
     return (
-      <div>
-        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"/>
-        {this.renderCampaigns()}
-        <Button
-          content='Create Campaign'
-          icon='add circle'
-          primary
-        />
-      </div>
+      <Layout>
+        <div>
+          <h2>Open Campaigns</h2>
+          <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"/>
+          {this.renderCampaigns()}
+          <Button
+            content='Create Campaign'
+            icon='add circle'
+            primary
+          />
+        </div>
+      </Layout>
     )
   }
 }
