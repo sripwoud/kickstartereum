72f117d819abd5ea2737070aca41bc54023e482f
sripwoud <me@sripwoud.xyz>
2019-09-30 15:58:57 +0200

    Add Header Component to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
+import Header from './Header'
+
 export default props => {
   return (
     <div>
-      <h1>Header</h1>
+      <Header />
       {props.children}
-      <h1>Footer</h1>
     </div>
   )
 }
