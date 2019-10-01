9a48e87f3414feeb4653219c18ff24f6dc3e60dd
sripwoud <me@sripwoud.xyz>
2019-10-01 23:33:07 +0200

    feat: Add route and page for showing payment requests

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,11 @@
+import react, { Component } from 'react'
+
+class RequestsIndex extends Component {
+  render () {
+    return (
+      <h3>Request list</h3>
+    )
+  }
+}
+
+export default RequestsIndex
