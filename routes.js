f8392bbb166289db3905e4df78ac885066692389
sripwoud <me@sripwoud.xyz>
2019-10-01 09:08:04 +0200

    feat: Show specific campaign
    Add dedicated route and component page

--- routes.js
+++ routes.js
@@ -1,3 +1,10 @@
 const routes = require('next-routes')() // calls function
 
+// this breaks the existing route 'campaign/new' (thinks new is an address!)
+// routes.add('/campaigns/:address', '/campaigns/show')
+
+routes
+  .add('/campaigns/new', '/campaigns/new')
+  .add('/campaigns/:address', 'campaigns/show')
+
 module.exports = routes
