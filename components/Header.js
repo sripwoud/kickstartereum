f9b5e85771412cf9ec94737b28221e543fadc8d0
sripwoud <me@sripwoud.xyz>
2019-10-01 23:00:13 +0200

    Update header and footer (links, styling)

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Menu } from 'semantic-ui-react'
+
 import { Link } from '../routes' // doesn't add HTML anchor tag, just wraps children with a click event handler
 
 export default () => {
   return (
     <Menu style={{ marginTop: '10px' }}>
       <Link route='/'>
-        <a className="item">
+        <a className='item'>
           AirCrowd
         </a>
       </Link>
       <Menu.Menu position='right'>
         <Link route='/'>
-          <a className="item">
+          <a className='item'>
             Campaigns
           </a>
         </Link>
         <Link route='/campaigns/new'>
-          <a className="item">
+          <a className='item'>
             +
           </a>
         </Link>
