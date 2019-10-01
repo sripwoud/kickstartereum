eee7ef816cc883dc8a80abe9340f2e886406b6a0
sripwoud <me@sripwoud.xyz>
2019-10-01 09:33:33 +0200

    Update Footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default () => {
   return (
-    <h3>Footer</h3>
+    <div>
+      <span>
+        <a href="https://github.com/6ry0u">
+        Developed by @6ry0u |
+        </a>
+      </span>
+      <span>
+        <a href="https://github.com/6ry0u/kickstartereum">
+          source code
+        </a>
+      </span>
+    </div>
   )
 }
