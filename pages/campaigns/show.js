9f6ab4a9f76edd091efc982beaa69c929fc9fbca
6ry0u <6ry0u@tutanota.com>
2019-10-01 23:33:07 +0200

    feat: Add route and page for showing payment requests

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { Card, Grid } from 'semantic-ui-react'
+import { Card, Grid, Button } from 'semantic-ui-react'
 
 import Layout from '../../components/Layout'
 import ContributeForm from '../../components/ContributeForm'
 import Campaign from '../../ethereum/campaign'
+import { Link } from '../../routes'
 
 // TODO: Redeploy contract!!
 class CampaignShow extends Component {
@@ -71,6 +72,11 @@ class CampaignShow extends Component {
         <Grid>
           <Grid.Column width={10}>
             {this.renderCards()}
+            <Link route={`/campaigns/${this.props.address}/requests`}>
+              <a>
+                <Button primary>View requests</Button>
+              </a>
+            </Link>
           </Grid.Column>
           <Grid.Column width={6}>
             <ContributeForm address={this.props.address} />
