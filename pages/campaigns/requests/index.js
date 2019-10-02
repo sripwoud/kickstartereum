7ee4d3f86140fbf698cd53f1fd6cbb1082a1c6d1
Gauthier Riou <gauthier.riou@airbus.com>
2019-10-02 16:01:32 +0200

    Create Row Component

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,15 +1,41 @@
-import react, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Table } from 'semantic-ui-react'
 
 import { Link } from '../../../routes'
 import Layout from '../../../components/Layout'
+import Campaign from '../../../ethereum/campaign'
+import RequestRow from '../../../components/RequestRow'
 
 class RequestsIndex extends Component {
-  static getInitialProps (props) {
+  static async getInitialProps (props) {
     const { address } = props.query
+    const campaign = Campaign(address)
+    const count = await campaign.methods.getRequestsCount().call()
+    const requests = await Promise.all(
+      Array(+count)
+        .fill()
+        .map((el, index) => {
+        return campaign.methods.requests(index).call()
+        })
+    )
+
     return { address }
   }
+
+  renderRows () {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          request={request}
+          address={address}
+        />
+      )
+    })
+  }
+
   render () {
+    const { Header, Row, HeaderCell, Body } = Table
     return (
       <Layout>
         <h2>Requests</h2>
@@ -20,6 +46,22 @@ class RequestsIndex extends Component {
             </Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>
+            {this.renderRows()}
+          </Body>
+        </Table>
       </Layout>
     )
   }
