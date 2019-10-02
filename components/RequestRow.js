9134af42badeba268ec10159d9b23ace5f4388b1
sripwoud <me@sripwoud.xyz>
2019-10-02 16:01:32 +0200

    Create Row Component

---
+++ components/RequestRow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RequestRow extends Component {
+
+  render() {
+    return (
+      <div>Request</div>
+    );
+  }
+
+}
+
+export default RequestRow;
