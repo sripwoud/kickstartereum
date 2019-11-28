d17aad2b76f756afbe2e2b057737289cca3adeab
6ry0u <6ry0u@tutanota.com>
2019-11-28 20:29:48 +0100

    Add divider informing that Metamask is required

--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,39 @@
 import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Menu, Divider } from 'semantic-ui-react'
 
 import { Link } from '../routes' // doesn't add HTML anchor tag, just wraps children with a click event handler
 
 export default () => {
   return (
-    <Menu style={{ marginTop: '10px' }}>
-      <Link route='/'>
-        <a className='item'>
-          Kickstartereum
-        </a>
-      </Link>
-      <Menu.Menu position='right'>
+    <>
+      <Menu style={{ marginTop: '10px' }}>
         <Link route='/'>
           <a className='item'>
+          Kickstartereum
+          </a>
+        </Link>
+        <Menu.Menu position='right'>
+          <Link route='/'>
+            <a className='item'>
             Campaigns
-          </a>
-        </Link>
-        <Link route='/campaigns/new'>
-          <a className='item'>
+            </a>
+          </Link>
+          <Link route='/campaigns/new'>
+            <a className='item'>
             +
-          </a>
-        </Link>
-      </Menu.Menu>
-    </Menu>
+            </a>
+          </Link>
+        </Menu.Menu>
+      </Menu>
+      <Divider horizontal>
+        <a hreh='https://metamask.io/'>
+          🦊 Metamask {' '}
+        </a>
+        is required to use this app
+        <a href='https://rinkeby.etherscan.io/address/0x1eb0ef281158ebccacf097e02111f3ab9e0743cc'>
+          {' '}(Rinkeby network) 🦊
+        </a>
+      </Divider>
+    </>
   )
 }
