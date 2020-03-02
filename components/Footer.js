54f1ca300ad3a66be2b8c9fd297fd30e0135eb9f
Gauthier Riou <r1oga@tuta.io>
2020-03-02 14:32:26 +0100

    Update Footer.js
    feat: Open links in new tabs

--- components/Footer.js
+++ components/Footer.js
@@ -7,18 +7,18 @@ export default () => {
       <Segment basic textAlign='center'>
         <p>
       Developed on
-          <a href='https://www.ethereum.org'>
+          <a href='https://www.ethereum.org' target='_blank'>
             <Icon name='ethereum' />
           </a>
       with {' '}
           <Icon name='like' color='red' padding='10px' />
       by
-          <a href='https://github.com/r1oga'>
+          <a href='https://github.com/r1oga' target='_blank'>
             @r1oga
           </a>
         </p>
         <p>
-          <a href='https://github.com/r1oga/kickstartereum'>
+          <a href='https://github.com/r1oga/kickstartereum' target='_blank'>
             <Icon name='github' size='large' color='black' />
           </a>
         </p>
