2c4c243e62c130250e3f660a69f30a876348782f
r1oga <r1oga@tuta.io>
2020-07-19 19:02:01 +0200

    Add HTML meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -10,9 +10,20 @@ export default props => {
     <Container>
       <Head>
         <meta property='og:title' content='Kickstartereum' />
-        <meta property='og:image' content='https://6ry0u.surge.sh/portfolio/kickstartereum.png' />
+        <meta
+          property='og:image'
+          content='https://6ry0u.surge.sh/portfolio/kickstartereum.png'
+        />
         <meta property='og:description' content='Kickstarter on Ethereum' />
-        <meta property='og:url' content='https://kickstartereum.herokuapp.com/' />
+        <meta
+          property='og:url'
+          content='https://kickstartereum.herokuapp.com/'
+        />
+        <meta
+          name='keywords'
+          content='Ethereum , Solidity, Smart Contract, Dapp, Kickstarter'
+        />
+        <meta name='author' content='r1oga' />
         <link
           rel='stylesheet'
           href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css'
