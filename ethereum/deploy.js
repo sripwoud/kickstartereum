158e390862616cebeb85fb4891019c45aabb5f16
r1oga <38692952+r1oga@users.noreply.github.com>
2022-11-21 15:21:16 +0100

    deploy on goerli

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -4,12 +4,18 @@ const Web3 = require('web3')
 const fs = require('fs-extra')
 
 const path = require('path')
-const { abi, evm: { bytecode: { object: bytecode } } } = require('./build/CampaignFactory.json')
+const {
+  abi,
+  evm: {
+    bytecode: { object: bytecode },
+  },
+} = require('./build/CampaignFactory.json')
 
-const provider = new HDWalletProvider(
-  process.env.PRIVATE_KEY,
-  `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
-)
+const provider = new HDWalletProvider({
+  privateKeys: [process.env.PRIVATE_KEY],
+  providerOrUrl: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+  chainId: 5,
+})
 const web3 = new Web3(provider)
 // define function so that we can use await/async syntax (optional)
 const deploy = async () => {
@@ -19,27 +25,30 @@ const deploy = async () => {
   try {
     const factory = await new web3.eth.Contract(abi)
       .deploy({ data: '0x' + bytecode })
-      .send({ from: accounts[0], gas: '1800000' })
+      .send({ from: accounts[0] })
 
     console.log('Contract deployed to', factory.options.address)
-    fs.readFile(
-      path.resolve(__dirname, '.env'),
-      'utf8',
-      (err, data) => {
-        if (!err) {
-          const splitArray = data.trim().split('\n')
-          if (splitArray.length === 3) {
-            splitArray.splice(2, 1)
-          }
-          splitArray.push(`ADDRESS=${factory.options.address}`)
-          const result = splitArray.join('\n')
-          fs.writeFile(path.resolve(__dirname, '.env'), result)
-        } else {
-          console.log(err)
+    fs.readFile(path.resolve(__dirname, '.env'), 'utf8', (err, data) => {
+      if (!err) {
+        const splitArray = data.trim().split('\n')
+        if (splitArray.length === 3) {
+          splitArray.splice(2, 1)
         }
-      })
+        splitArray.push(`ADDRESS=${factory.options.address}`)
+        const result = splitArray.join('\n')
+        fs.appendFileSync(path.resolve(__dirname, '.env'), result)
+      } else {
+        console.log(err)
+      }
+    })
   } catch (e) {
     console.log('Failed to deploy:', e.message)
   }
 }
+
 deploy()
+  .then(() => {
+    console.log('done')
+    process.exit(0)
+  })
+  .catch((e) => console.error(e))
